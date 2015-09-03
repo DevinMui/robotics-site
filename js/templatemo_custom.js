4ef83e2325e4f387e8c72b9cf44ed808d0712243
Devin <devinmui@yahoo.com>
2015-09-02 17:02:40 -0700

    fixed js issue with the team thing

--- js/templatemo_custom.js
+++ js/templatemo_custom.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function(){
 
-	//$('#service_tabs li:first-child').tab('show');
+	$('#service_tabs li:first-child').tab('show');
 
 	$('#services .services_buttons li').each( function(){
 		$(this).on('click', function(){
@@ -20,20 +20,3 @@ function change_panels(new_index){
 	$('.services_buttons li').removeClass('active');
 	$('.services_buttons li:nth-child('+(new_index+1)+')').addClass('active');
 }
-
-var map = '';
-
-function initialize() {
-    var mapOptions = {
-      zoom: 14,
-      center: new google.maps.LatLng(16.8461789,96.1309764)
-    };
-    map = new google.maps.Map(document.getElementById('google_map'),  mapOptions);
-}
-
-// load google map
-var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&' +
-        'callback=initialize';
-    document.body.appendChild(script);
